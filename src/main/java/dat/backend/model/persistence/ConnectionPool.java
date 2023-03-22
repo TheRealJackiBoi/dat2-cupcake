7607effbca02a8f8dedf7ed074b5c277cc67b776
william hansen <williamemil@live.dk>
2023-03-22 14:03:12 +0100

    loginpage og createuser

--- src/main/java/dat/backend/model/persistence/ConnectionPool.java
+++ src/main/java/dat/backend/model/persistence/ConnectionPool.java
@@ -15,7 +15,7 @@ public class ConnectionPool
     private HikariDataSource ds;
     private static String USER = "root";
     private static String PASSWORD = "root";
-    private static String URL = "jdbc:mysql://localhost:3306/startcode";
+    private static String URL = "jdbc:mysql://localhost:3306/fourthingsplus";
 
     public ConnectionPool()
     {
